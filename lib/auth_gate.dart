1717abe6afae7b31d975f854bed45f5391c9ed18
sape69 <52237862+sape69@users.noreply.github.com>
2026-08-27 07:26:33 +0300

    Create auth_gate.dart

---
+++ lib/auth_gate.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'pages/home_page.dart';
+import 'pages/loading_page.dart';
+import 'pages/login_page.dart';
+
+class AuthGate extends StatelessWidget {
+  final String languageCode;
+  final Future<void> Function(String) changeLanguage;
+
+  const AuthGate({
+    super.key,
+    required this.languageCode,
+    required this.changeLanguage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const LoadingPage();
+        }
+
+        if (snapshot.hasData) {
+          return HomePage(
+            languageCode: languageCode,
+            changeLanguage: changeLanguage,
+          );
+        }
+
+        return LoginPage(
+          languageCode: languageCode,
+          changeLanguage: changeLanguage,
+        );
+      },
+    );
+  }
+}
