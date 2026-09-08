5af3a50bbb3f68f7cbe6725d44b5ce6e2b8ae103
sape69 <52237862+sape69@users.noreply.github.com>
2026-09-08 08:19:27 +0300

    Create lib/localization/whitepaper/whitepaper_fi.dart

---
+++ lib/localization/whitepaper/whitepaper_fi.dart
@@ -0,0 +1,226 @@
+const Map<String, String> whitepaperFi = {
+  'pageTitle': 'VALKOINEN KIRJA',
+  'communityToken': '🐾 SOLANA-YHTEISÖTOKEN 🐾',
+  'whitePaper': 'VALKOINEN KIRJA',
+  'version': 'Versio 1.0',
+
+  '01_title': 'Tiivistelmä',
+  '01_text':
+      'Stelluriini on yhteisölähtöinen digitaalinen projekti, joka rakentuu uteliaan Stella-kissan ympärille. Stella edustaa luovuutta, yhteisöllisyyttä ja tutkimista. STL on projektin token Solana-lohkoketjussa. Stelluriini-sovellus yhdistää Stellan brändin, louhintatyyliset palkkiomekaniikat, päivittäiset aktiviteetit ja STL-ekosysteemiä koskevan tiedon.',
+
+  '02_title': 'Visio',
+  '02_text':
+      'Stelluriinin visiona on luoda tunnistettava ja yhteisökeskeinen digitaalinen ekosysteemi, jossa Stella on kokemuksen keskiössä.',
+  '02_b1': 'Rakentaa vahva ja tunnistettava Stella-identiteetti.',
+  '02_b2':
+      'Luoda mukaansatempaavia sovelluksia ja digitaalisia kokemuksia.',
+  '02_b3': 'Kasvattaa aktiivista ja tervetullutta yhteisöä.',
+  '02_b4':
+      'Kehittää hyödyllisiä ja viihdyttäviä STL-ekosysteemin ominaisuuksia.',
+  '02_b5':
+      'Tutkia pelejä, sovelluksia ja tulevia Solana-integraatioita.',
+
+  '03_title': 'Mikä Stelluriini on?',
+  '03_text':
+      'Stelluriini on enemmän kuin tokenin nimi. Se on Stella-kissan ympärille rakennettu projektin identiteetti ja yhteisölähtöinen digitaalinen kokemus.',
+  '03_f1_title': 'Stella',
+  '03_f1_desc':
+      'Projektin visuaalinen maskotti ja tunnistettava identiteetti.',
+  '03_f2_title': 'STL',
+  '03_f2_desc':
+      'Stelluriini-token, joka liittyy Solana-ekosysteemiin.',
+  '03_f3_title': 'Sovellus',
+  '03_f3_desc':
+      'Mobiilikokemus, joka sisältää louhintatyylisiä palkintoja, päivittäisiä aktiviteetteja ja projektitietoa.',
+  '03_f4_title': 'Yhteisö',
+  '03_f4_desc':
+      'Yhteisökeskeinen ympäristö, jossa tulevia ominaisuuksia voidaan kehittää yhdessä.',
+
+  '04_title': 'Stella',
+  '04_heading': 'Stella on Stelluriinin sydän.',
+  '04_text':
+      'Stella edustaa uteliaisuutta, ystävällisyyttä ja tutkimista. Hänen tehtävänsä on tehdä Stelluriini-kokemuksesta tunnistettava ja tarjota yhtenäinen identiteetti sovellukselle, yhteisölle ja tulevalle ekosysteemille.',
+
+  '05_title': 'STL Token',
+  '05_name': 'Tokenin nimi',
+  '05_symbol': 'Symboli',
+  '05_blockchain': 'Lohkoketju',
+  '05_supply': 'Kokonaistarjonta',
+  '05_decimals': 'Desimaalit',
+  '05_mint': 'Mint-osoite',
+
+  '06_title': 'Tokenomiikka',
+  '06_text':
+      'Suunniteltu STL-allokaatio on tarkoitettu tukemaan yhteisöpalkintoja, likviditeettiä, ekosysteemin kasvua, kehitystä ja markkinointia.',
+  '06_a1': 'Yhteisö & palkinnot',
+  '06_a2': 'Likviditeetti',
+  '06_a3': 'Ekosysteemi',
+  '06_a4': 'Kehitys',
+  '06_a5': 'Markkinointi',
+  '06_total': 'Allokaatio yhteensä: 17 602 539 062 STL',
+
+  '07_title': 'Stella Mining',
+  '07_text':
+      'Stelluriini-sovellus sisältää louhintatyylisen palkintojärjestelmän. Järjestelmä on suunniteltu sovelluksen sisäiseksi etenemismekaniikaksi, jossa käyttäjät keräävät virtuaalisia STL-pisteitä louhintajakson aikana.',
+  '07_f1_title': 'Hash Rate',
+  '07_f1_desc':
+      'Käyttäjän hash rate määrittää nopeuden, jolla virtuaalisia louhintapisteitä kertyy.',
+  '07_f2_title': 'Louhintajakso',
+  '07_f2_desc':
+      'Louhintajakso kestää määritellyn ajan, minkä jälkeen kertynyt palkinto voidaan kerätä.',
+  '07_f3_title': 'Palkinnon laskenta',
+  '07_f3_desc':
+      'Virtuaalinen palkinto lasketaan hash raten ja kuluneen louhinta-ajan perusteella.',
+  '07_f4_title': 'Lukittu louhintanopeus',
+  '07_f4_desc':
+      'Aktiivisen louhintajakson käyttämä hash rate pysyy vakaana kyseisen jakson ajan.',
+
+  '08_title': 'Päivittäinen bonus',
+  '08_text':
+      'Päivittäinen bonus kannustaa säännölliseen osallistumiseen Stelluriini-sovelluksessa. Onnistunut päivittäinen kirjautuminen voi kasvattaa käyttäjän hash ratea ja ylläpitää peräkkäisten päivien putkea.',
+  '08_b1':
+      'Päivittäisen bonuksen voi lunastaa onnistuneesti kerran päivässä.',
+  '08_b2':
+      'Peräkkäisten päivien putkea voidaan ylläpitää palaamalla seuraavina päivinä.',
+  '08_b3':
+      'Bonus vaikuttaa käyttäjän hash rateen, jota käytetään tulevissa louhintajaksoissa.',
+
+  '09_title': 'Stella Power Boost',
+  '09_text':
+      'Stella Power Boost -järjestelmän avulla käyttäjät voivat saada ylimääräisen hash rate -bonuksen katsomalla palkintomainoksen. Järjestelmässä on rajoituksia ja jäähdytysaikoja väärinkäytösten vähentämiseksi.',
+  '09_f1_title': 'Palkintomainokset',
+  '09_f1_desc':
+      'Käyttäjä voi saada sovelluksen sisäisen hash rate -bonuksen hyväksytyn palkintomainoksen jälkeen.',
+  '09_f2_title': 'Jäähdytysaika',
+  '09_f2_desc':
+      'Jäähdytysaika rajoittaa sitä, kuinka usein mainospalkinnon voi lunastaa.',
+  '09_f3_title': 'Päivittäinen raja',
+  '09_f3_desc':
+      'Päivässä voidaan laskea mukaan enintään määritelty määrä palkintomainoksia.',
+
+  '10_title': 'Sovellusarkkitehtuuri',
+  '10_text':
+      'Stelluriini-sovellus on suunniteltu mobiiliasiakkaan ja palvelinpuolen palveluiden ympärille. Palvelinpalvelut käsittelevät tunnistettuja palkintotoimintoja ja tapahtumahistoriaa.',
+  '10_b1_title': 'Flutter-sovellus',
+  '10_b1_desc':
+      'Käyttöliittymä, Stella-kokemus, louhintanäkymä ja projektitiedot.',
+  '10_b2_title': 'Firebase-palvelut',
+  '10_b2_desc':
+      'Autentikointi, Firestore-tiedot ja palvelinpuolen Cloud Functions -toiminnot.',
+  '10_b3_title': 'Palvelinpuolen validointi',
+  '10_b3_desc':
+      'Palkintorajat, jäähdytysajat, kaksoiskäsittelyn esto ja tunnistetut toiminnot.',
+  '10_b4_title': 'Solana / STL',
+  '10_b4_desc':
+      'Stelluriini-token on Solana-lohkoketjussa oleva digitaalinen omaisuuserä.',
+
+  '11_title': 'Tapahtumahistoria',
+  '11_text':
+      'Sovellus tarjoaa tapahtumahistorianäkymän, jossa käyttäjälle voidaan näyttää tallennettua palkintoihin liittyvää toimintaa.',
+  '11_b1': 'Louhintapalkintojen tapahtumat.',
+  '11_b2': 'Päivittäisten palkintojen tapahtumat.',
+  '11_b3': 'Palkintomainosten tapahtumat.',
+  '11_b4': 'Saldo tallennetun palkinnon jälkeen.',
+  '11_b5': 'Tapahtuman päivämäärä ja tyyppi.',
+
+  '12_title': 'Turvallisuus & väärinkäytösten esto',
+  '12_text':
+      'Stelluriini-sovellus käyttää palvelinpuolen validointia palkintotoimintojen manipuloinnin vähentämiseksi. Turvamekanismien tarkoituksena on suojata sovelluksen ja palkintojärjestelmän eheyttä.',
+  '12_f1_title': 'Autentikointi',
+  '12_f1_desc':
+      'Palkintotoiminnot edellyttävät tunnistettua käyttäjäistuntoa.',
+  '12_f2_title': 'Kaksoiskäsittelyn esto',
+  '12_f2_desc':
+      'Palkintotapahtumat voidaan suojata toistuvaa käsittelyä vastaan.',
+  '12_f3_title': 'Käyttörajoitukset',
+  '12_f3_desc':
+      'Päivittäiset rajat ja jäähdytysajat auttavat vähentämään automatisoitua väärinkäyttöä.',
+  '12_f4_title': 'Palkintojen validointi',
+  '12_f4_desc':
+      'Palvelinpuolen logiikka tarkistaa tärkeät palkintoehdot ennen niiden tallentamista.',
+
+  '13_title': 'Yhteisö',
+  '13_text':
+      'Yhteisön osallistuminen on tärkeä osa Stelluriinin visiota. Projektin tavoitteena on kehittää ympäristöä, jossa käyttäjät voivat seurata kehitystä, antaa palautetta ja osallistua tuleviin ekosysteemin aktiviteetteihin.',
+  '13_b1':
+      'Yhteisön palaute voi vaikuttaa tulevaan kehitykseen.',
+  '13_b2':
+      'Tulevat yhteisöominaisuudet voivat laajentaa STL:n roolia.',
+  '13_b3':
+      'Läpinäkyvyys on tarkoitus pitää tärkeänä projektin periaatteena.',
+
+  '14_title': 'Tuleva ekosysteemi',
+  '14_text':
+      'Tuleva Stelluriini-ekosysteemi voi laajentua nykyisen sovelluksen ulkopuolelle. Mahdollisia suuntia ovat uudet Stella-kokemukset, pelit, sovellukset, yhteisöominaisuudet ja lisäintegraatiot Solanan kanssa.',
+  '14_f1_title': 'Stella-pelit',
+  '14_f1_desc':
+      'Tutkitaan pelejä ja interaktiivisia kokemuksia, joissa Stella esiintyy.',
+  '14_f2_title': 'Uudet sovellukset',
+  '14_f2_desc':
+      'Kehitetään uusia digitaalisia tuotteita ja palveluita Stelluriini-identiteetin ympärille.',
+  '14_f3_title': 'STL-integraatiot',
+  '14_f3_desc':
+      'Tutkitaan hyödyllisiä STL:n ja Solana-ekosysteemin integraatioita.',
+  '14_f4_title': 'Yhteisöaktiviteetit',
+  '14_f4_desc':
+      'Mahdollisia tapahtumia, haasteita ja yhteisökeskeisiä palkintokokemuksia.',
+
+  '15_title': 'Tiekartta',
+  '15_p1_title': 'Alku',
+  '15_p1_status': 'KÄYNNISSÄ',
+  '15_p1_desc':
+      'Stelluriini-identiteetin luominen, Stella-brändin kehittäminen, sovelluksen rakentaminen ja STL-tietojen valmistelu.',
+  '15_p2_title': 'Yhteisö',
+  '15_p2_status': 'SUUNNITELTU',
+  '15_p2_desc':
+      'Yhteisön kasvattaminen, kielituen parantaminen, päivittäisten aktiviteettien ja yhteisöominaisuuksien kehittäminen.',
+  '15_p3_title': 'STL-ekosysteemi',
+  '15_p3_status': 'TULEVAISUUS',
+  '15_p3_desc':
+      'STL-ekosysteemin toiminnallisuuden, lohkoketjutietojen, tilastojen ja muiden Stelluriini-ominaisuuksien laajentaminen.',
+  '15_p4_title': 'Tulevaisuus',
+  '15_p4_status': 'TULEVAISUUS',
+  '15_p4_desc':
+      'Ekosysteemin jatkuva kehittäminen, uusien Stella-kokemusten tuominen ja STL:n uusien mahdollisuuksien tutkiminen.',
+
+  '16_title': 'Läpinäkyvyys',
+  '16_text':
+      'Stelluriinin tavoitteena on erottaa selkeästi olemassa oleva toiminnallisuus tulevaisuuden suunnitelmista. Projektidokumentaation tarkoituksena on kuvata projektin nykytila ja suunniteltu suunta mahdollisimman tarkasti.',
+  '16_b1': 'STL:n kokonaistarjonta on dokumentoitu.',
+  '16_b2': 'Token-allokaation rakenne on dokumentoitu.',
+  '16_b3':
+      'Nykyinen sovellustoiminnallisuus kuvataan erillään tulevaisuuden suunnitelmista.',
+  '16_b4':
+      'Tiekartan painopisteet voivat muuttua kehityksen edetessä.',
+
+  '17_title': 'Riskit & rajoitukset',
+  '17_text':
+      'Digitaaliset omaisuuserät ja ohjelmistoprojektit sisältävät teknisiä, markkina-, sääntely- ja toiminnallisia riskejä. Käyttäjien tulee ymmärtää nämä riskit ennen lohkoketjupohjaisen omaisuuserän tai sovelluksen käyttöä.',
+  '17_b1': 'Kryptovaluuttamarkkinat voivat olla erittäin epävakaita.',
+  '17_b2':
+      'Lohkoketjutapahtumat voivat sisältää peruuttamattomia toimintoja.',
+  '17_b3':
+      'Ohjelmistoissa voi olla virheitä tai teknisiä rajoituksia.',
+  '17_b4':
+      'Lohkoketju- ja sääntely-ympäristöt voivat muuttua.',
+  '17_b5': 'Tulevia tiekartan tavoitteita ei taata.',
+  '17_b6':
+      'Sovelluksen virtuaalisia palkintoja ei tule tulkita taatuiksi taloudellisiksi tuotoiksi.',
+
+  '18_title': '18 • VASTUUVAPAUSLAUSEKE',
+  '18_text':
+      'Stelluriini ja STL esitellään yhteisölähtöisenä digitaalisena projektina. Tämän asiakirjan tiedot on tarkoitettu ainoastaan informatiivisiin tarkoituksiin eivätkä ne muodosta taloudellista, sijoitus-, oikeudellista tai veroneuvontaa.',
+  '18_text2':
+      'Sovelluksessa näkyviä virtuaalisia pisteitä ei tule tulkita taatuksi kryptovaluutan arvoksi tai taatuksi taloudelliseksi tuotoksi.',
+
+  '19_title': 'Viralliset tiedot',
+  '19_text':
+      'Virallista Stelluriini-sovellusta tulee käyttää yhdessä varmennettujen projektitietojen kanssa. Tarkista token-osoitteet aina ennen lohkoketjuomaisuuserien kanssa toimimista.',
+  '19_mint': 'STELLURIINI MINT',
+
+  'closing': '🐱 Stella on vasta aloittamassa.',
+  'closingSub': 'Yhteisö • Uteliaisuus • Luovuus • Solana',
+  'footer': '🐾 STELLA • STELLURIINI • STL • SOLANA 🐾',
+  'footerSupply': '17 602 539 062 STL',
+  'footerVersion': 'VALKOINEN KIRJA v1.0',
+};
