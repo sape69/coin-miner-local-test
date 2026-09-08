1f21c0509ea76149e378b702da86a504b4f913f7
sape69 <52237862+sape69@users.noreply.github.com>
2026-09-08 07:00:34 +0300

    Update home_drawer.dart

--- lib/widgets/home_drawer.dart
+++ lib/widgets/home_drawer.dart
@@ -21,7 +21,6 @@ class HomeDrawer extends StatelessWidget {
   final VoidCallback onLanguagePressed;
   final VoidCallback onAboutPressed;
   final VoidCallback onWhitePaperPressed;
-  final VoidCallback onTokenomicsPressed;
   final VoidCallback onRoadmapPressed;
   final VoidCallback onTransactionHistoryPressed;
 
@@ -30,7 +29,6 @@ class HomeDrawer extends StatelessWidget {
     required this.onLanguagePressed,
     required this.onAboutPressed,
     required this.onWhitePaperPressed,
-    required this.onTokenomicsPressed,
     required this.onRoadmapPressed,
     required this.onTransactionHistoryPressed,
   });
@@ -279,12 +277,6 @@ class HomeDrawer extends StatelessWidget {
                     icon: Icons.description_outlined,
                     title: 'White Paper',
                     onTap: onWhitePaperPressed,
-                  ),
-
-                  _menuItem(
-                    icon: Icons.pie_chart_outline_rounded,
-                    title: 'Tokenomics',
-                    onTap: onTokenomicsPressed,
                   ),
 
                   _menuItem(
