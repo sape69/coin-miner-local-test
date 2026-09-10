59fc53e0802bc3e0406c368c6acf49cb90f6ba06
sape69 <52237862+sape69@users.noreply.github.com>
2026-09-10 19:41:03 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'pages/login_page.dart';
 
-const Color backgroundColor = Color(0xFF120B24);
+const Color backgroundColor =
+    Color(0xFF120B24);
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp();
+
   runApp(
-    const StelluriiniTestApp(),
+    const StelluriiniApp(),
   );
 }
 
-class StelluriiniTestApp extends StatelessWidget {
-  const StelluriiniTestApp({
+class StelluriiniApp extends StatelessWidget {
+  const StelluriiniApp({
     super.key,
   });
 
@@ -25,15 +29,28 @@ class StelluriiniTestApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.dark,
         useMaterial3: true,
-        scaffoldBackgroundColor: backgroundColor,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFFB58CFF),
-          brightness: Brightness.dark,
+        scaffoldBackgroundColor:
+            backgroundColor,
+        colorScheme:
+            ColorScheme.fromSeed(
+          seedColor:
+              const Color(0xFFB58CFF),
+          brightness:
+              Brightness.dark,
+          primary:
+              const Color(0xFFB58CFF),
+          secondary:
+              const Color(0xFFFFB7E8),
+          tertiary:
+              const Color(0xFFFFD166),
+          surface:
+              const Color(0xFF1A0E31),
         ),
       ),
       home: LoginPage(
         languageCode: 'fi',
-        changeLanguage: (String language) async {},
+        changeLanguage:
+            (String language) async {},
       ),
     );
   }
