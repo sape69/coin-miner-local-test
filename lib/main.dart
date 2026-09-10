cbe3eebed7bafca617e8524a4ad92930d0999081
sape69 <52237862+sape69@users.noreply.github.com>
2026-09-10 20:13:39 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import 'auth_gate.dart';
 
@@ -10,6 +11,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
+
+  await MobileAds.instance.initialize();
 
   runApp(
     const StelluriiniApp(),
