0ff47b60f67218f5216902a080fbfd1aecb6cdf3
sape69 <52237862+sape69@users.noreply.github.com>
2026-09-12 19:38:38 +0300

    Update fi.dart

--- lib/localization/languages/fi.dart
+++ lib/localization/languages/fi.dart
@@ -691,9 +691,6 @@ const Map<String, String> fiTranslations = {
   'communityCuriositySolana':
       'Yhteisö • Uteliaisuus • Solana',
 
-  'stelluriiniStlSolanaFooter':
-      'STELLURIINI • STL • SOLANA',
-
   // ============================================================
   // 📊 TOKENOMICS
   // ============================================================
