ca943565a087b02e7c3a8ce7d2e815b2ffcc479f
sape69 <52237862+sape69@users.noreply.github.com>
2026-09-26 02:12:43 +0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/stelluriini/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/stelluriini/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.example.stelluriini
+package com.stelluriini.app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
